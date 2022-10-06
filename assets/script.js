975e2c285ef0baf68c69dff07397132c7c676915
Jonny G <110258278+jonathanlevi14@users.noreply.github.com>
2022-10-06 15:54:01 -0700

    got the hour component to work

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,9 @@
 //global variables
 var time = moment().format("HH:mm:ss");
-var timeOfDay = $(".time-of-day")
-console.log(timeOfDay)
+var timeOfDay = $(".time-of-day");
+var currentHour = parseInt(moment().format("H"))
+var text8 = $("#text-8");
+// console.log(timeOfDay)
 
 //===========================================================
 //displays currnet day/month on scheduler
@@ -12,40 +14,39 @@ $("#currentDay").text(currentDay);
 //for loop and if statment for time being past or future
 //get current hour from moment 
 function hourUpdater() {
-  for (let i = 0; i < timeOfDay.length; i++) {
-    if (time >= timeOfDay[i]) {
-      console.log(timeOfDay)
-      changeColorGray();
-    }
+  for (let i = 8; i < 17; i++) {
+    // console.log(currentHour, i)
+    var currentRow = document.querySelector(`#text-${i}`)
+    if (currentHour === i) {
+      currentRow.classList.add("present");
+  } else if (currentHour < i) {
+      // future
+      currentRow.classList.add("future")
+      //past
+  } else  (currentHour > i)
+    currentRow.classList.add("past")
   }
-    // var  allClassPresent = $(".present");
-    // $(".container").find( allClassPresent).attr("present", "past");
-
-    document.querySelector(".event").classList.remove("present");
-    document.querySelector(".event").classList.add("past");
-
-    // function chnageColorGreen() {
-    //   document.querySelector(".event").classList.remove("present");
-    //   document.querySelector(".event").classList.add("future");}
 }
-hourUpdater()
-//==========================================================
-//click event to be able to enter
 
-$("").click(function () {
-  const input = document.createElement("input");
-  input.setAttribute("type", "text");
-  // document.body.appendChild(input);
-  console.log("these will trigger an input box, possibly modal?");
-});
 
 //=========================================================
 //click save button to save event to local storage
 //
-var dailyActivity = $(".saveBtn").click(function () {
-  console.log("This will save the document in localstorage");
+function saveInfo() {
+  var dailyActivity = text8.val()
   localStorage.setItem("activity", dailyActivity);
-});
+}
 
-//=========================================================
-//when i refresh the page the saved event presists
+function retrieveInfo() {
+  var savedInfo = localStorage.getItem("activity")
+  text8.text(savedInfo);
+   var dailyActivity = text8.val()
+  localStorage.setItem("activity", dailyActivity);
+}
+
+
+
+$(".saveBtn").click(saveInfo);
+retrieveInfo()
+hourUpdater()
+//===============================================
