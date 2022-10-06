320286642514b90937bb4328db5e1c77699e5a72
Jonny G <110258278+jonathanlevi14@users.noreply.github.com>
2022-10-06 10:02:41 -0700

    made changes to the hourUpdater

--- assets/script.js
+++ assets/script.js
@@ -1,58 +1,51 @@
 //global variables
-var time = moment().format("HH:mm:ss")
-var endOfHour = ["08:59:59","09:59:59","10:59:59","11:59:59","12:59:59","13:59:59","14:59:59","15:59:59","16:59:59"]
-var startOfHour = ["8:00:00","9:00:00","10:00:00","11:00:00","12:00:00","13:00:00","14:00:00","15:00:00","16:00:00","17:00:00"]
+var time = moment().format("HH:mm:ss");
+var timeOfDay = $(".time-of-day")
+console.log(timeOfDay)
 
 //===========================================================
 //displays currnet day/month on scheduler
-var currentDay = moment().format("ddd MMM Do")
+var currentDay = moment().format("ddd MMM Do");
 $("#currentDay").text(currentDay);
 
 //===========================================================
 //for loop and if statment for time being past or future
-for (var j = 0; j < startOfHour.length; j++) {
- if (time <= startOfHour[j]){
-    // chnageColorGreen();
- }}
+//get current hour from moment 
+function hourUpdater() {
+  for (let i = 0; i < timeOfDay.length; i++) {
+    if (time >= timeOfDay[i]) {
+      console.log(timeOfDay)
+      changeColorGray();
+    }
+  }
+    // var  allClassPresent = $(".present");
+    // $(".container").find( allClassPresent).attr("present", "past");
 
-for (let i = 0; i < endOfHour.length; i++) {
- if (time >= endOfHour[i]) {
-    changeColorGray();
-  }}
-function changeColorGray() {
-  // var  allClassPresent = $(".present");
-  // $(".container").find( allClassPresent).attr("present", "past");
+    document.querySelector(".event").classList.remove("present");
+    document.querySelector(".event").classList.add("past");
 
-  document.querySelector(".event").classList.remove("present");
-  document.querySelector(".event").classList.add("past");
-
-// function chnageColorGreen() {
-  // document.querySelector(".event").classList.remove("present");
-  // document.querySelector(".event").classList.add("future");}
+    // function chnageColorGreen() {
+    //   document.querySelector(".event").classList.remove("present");
+    //   document.querySelector(".event").classList.add("future");}
 }
-
+hourUpdater()
 //==========================================================
-//click event to be able to enter 
+//click event to be able to enter
 
-var dailyActivity = 
-
-  
-  $( ".target" ).click(function() {
-    const input = document.createElement("input");
-    input.setAttribute("type", "text");
-    // document.body.appendChild(input);
-   console.log("these will trigger an input box, possibly modal?")
-  });
+$("").click(function () {
+  const input = document.createElement("input");
+  input.setAttribute("type", "text");
+  // document.body.appendChild(input);
+  console.log("these will trigger an input box, possibly modal?");
+});
 
 //=========================================================
 //click save button to save event to local storage
 //
-$( ".saveBtn" ).click(function() {
-  console.log("This will save the document in localstorage")
-localStorage.setItem("activity", dailyActivity)
+var dailyActivity = $(".saveBtn").click(function () {
+  console.log("This will save the document in localstorage");
+  localStorage.setItem("activity", dailyActivity);
 });
-
-
 
 //=========================================================
 //when i refresh the page the saved event presists
