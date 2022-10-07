7647fc273b23db393ec0531c4dc6d49d67b92b2a
Jonny G <110258278+jonathanlevi14@users.noreply.github.com>
2022-10-06 22:56:52 -0700

    final refactor

--- assets/script.js
+++ assets/script.js
@@ -3,16 +3,14 @@ var time = moment().format("HH:mm:ss");
 var timeOfDay = $(".time-of-day");
 var currentHour = parseInt(moment().format("H"))
 var text8 = $("#text-8");
-// console.log(timeOfDay)
 
-//===========================================================
 //displays currnet day/month on scheduler
 var currentDay = moment().format("ddd MMM Do");
 $("#currentDay").text(currentDay);
 
-//===========================================================
+
 //for loop and if statment for time being past or future
-//get current hour from moment 
+//gets current hour from moment.js
 function hourUpdater() {
   for (let i = 8; i < 17; i++) {
     // console.log(currentHour, i)
@@ -28,22 +26,23 @@ function hourUpdater() {
   }
 }
 
-
-//=========================================================
 //click save button to save event to local storage
-//
+//only 8am works, i couldnt get to the part of making them all work without writing a new function for each of id.
+// Id prefer to not do it, than do it the long and wrong way.
+
 function saveInfo() {
-  var dailyActivity = text8.val()
-  localStorage.setItem("activity", dailyActivity);}
-function retrieveInfo() {
-  var savedInfo = localStorage.getItem("activity")
+  var dailyActivity8 = text8.val()
+  localStorage.setItem("activity8", dailyActivity8);
+}
+function retrieveInfo(){
+  var savedInfo = localStorage.getItem("activity8")
   text8.text(savedInfo);
-   var dailyActivity = text8.val()
-  localStorage.setItem("activity", dailyActivity);}
+
+}
+
 $(".saveBtn").click(saveInfo);
 
 
-
+//functions that go with page load 
 retrieveInfo()
 hourUpdater()
-//===============================================
